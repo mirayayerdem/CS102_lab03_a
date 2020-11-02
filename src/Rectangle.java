42cc99db72db5a69bbaa903270ebecaf697c8f10
miray <mirayayerdem@gmail.com>
2020-11-02 21:29:34 +0300

    Rectangle Class is created

---
+++ src/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle extends Shape {
+    public Rectangle (int sides, int width, int height)
+    {
+
+    }
+    public double getArea()
+    {
+
+    }
+    public double getPerimeter()
+    {
+
+    }
+}
