efe92453ce891700b9bb00fa2d092f3a36cee0c8
miray <mirayayerdem@gmail.com>
2020-11-03 18:46:35 +0300

    Locatable Class is modified

--- src/Locatable.java
+++ src/Locatable.java
@@ -1,4 +1,10 @@
+/**
+ * an interface model to provide location for shapes
+ * author: Miray Ayerdem
+ * version : 03/11/2020
+ */
 public interface Locatable {
+    //methods
     public int getX();
     public int getY();
     public void setLocation(int x, int y);
