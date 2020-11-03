b53c698bfd9cff49d0851c242f665a558d6d7b1e
miray <mirayayerdem@gmail.com>
2020-11-03 18:48:03 +0300

    Selectable Class is modified

--- src/Selectable.java
+++ src/Selectable.java
@@ -1,4 +1,10 @@
+/**
+ * an interface model for selecting objects
+ * author: Miray Ayerdem
+ * version : 03/11/2020
+ */
 public interface Selectable {
+    //methods
     public boolean getSelected();
     public void setSelected(boolean a);
     public Shape contains(int x, int y);
